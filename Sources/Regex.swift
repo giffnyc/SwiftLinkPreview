01d1f738af8a3edbb822e0fd05985701f17c3299
David Gifford <giff@ncirql.com>
2017-03-20 16:40:21 -0400

    Using an improved URL Regex, also allowing URLSs that have no protocol defined.

--- Sources/Regex.swift
+++ Sources/Regex.swift
@@ -16,7 +16,46 @@ class Regex {
     static let metatagPattern = "<meta(.*?)>"
     static let metatagContentPattern = "content=(\"(.*?)\")|('(.*?)')"
     static let cannonicalUrlPattern = "([^\\+&#@%\\?=~_\\|!:,;]+)"
-    static let rawUrlPattern = "((http[s]?|ftp|file)://)?((([-a-zA-Z0-9]+\\.)|\\.)+[-a-zA-Z0-9]+)[-a-zA-Z0-9+&@#/%?=~_|!:,\\.;]*"
+//    static let rawUrlPattern = "((http[s]?|ftp|file)://)?((([-a-zA-Z0-9]+\\.)|\\.)+[-a-zA-Z0-9]+)[-a-zA-Z0-9+&@#/%?=~_|!:,\\.;]*"
+
+    static let rawUrlPattern =
+        "^" +
+            // protocol identifier
+            "(?:(?:https?|ftp)://)?" +
+            // user:pass authentication
+            "(?:\\S+(?::\\S*)?@)?" +
+            "(?:" +
+            // IP address exclusion
+            // private & local networks
+            "(?!(?:10|127)(?:\\.\\d{1,3}){3})" +
+            "(?!(?:169\\.254|192\\.168)(?:\\.\\d{1,3}){2})" +
+            "(?!172\\.(?:1[6-9]|2\\d|3[0-1])(?:\\.\\d{1,3}){2})" +
+            // IP address dotted notation octets
+            // excludes loopback network 0.0.0.0
+            // excludes reserved space >= 224.0.0.0
+            // excludes network & broacast addresses
+            // (first & last IP address of each class)
+            "(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])" +
+            "(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}" +
+            "(?:\\.(?:[1-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))" +
+            "|" +
+            // host name
+            "(?:(?:[a-z\u{00a1}-\u{ffff}0-9]-*)*[a-z\u{00a1}-\u{ffff}0-9]+)" +
+            // domain name
+            "(?:\\.(?:[a-z\u{00a1}-\u{ffff}0-9]-*)*[a-z\u{00a1}-\u{ffff}0-9]+)*" +
+            // TLD identifier
+            "(?:\\.(?:[a-z\u{00a1}-\u{ffff}]{2,}))" +
+            // TLD may end with dot
+            "\\.?" +
+            ")" +
+            // port number
+            "(?::\\d{2,5})?" +
+            // resource path
+            "(?:[/?#]\\S*)?" +
+    "$"
+    
+    
+    
     static let rawTagPattern = "<[^>]+>"
     static let inlineStylePattern = "<style(.*?)>(.*?)</style>"
     static let inlineScriptPattern = "<script(.*?)>(.*?)</script>"
